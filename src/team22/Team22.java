945259618f300ae4c85af464360bf2868633998c
Andrew Maher <andrewaacodo12@gmail.com>
2018-12-01 19:44:43 +0200

    Resolve Conflict

--- src/team22/Team22.java
+++ src/team22/Team22.java
@@ -1,64 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package team22;
 
-
-
-/**
- *
- * @author AndrOoO
- */
 import java.util.Random;
 import java.util.Scanner;
 import java.util.*;
-public class Team22 {
+import javax.swing.JOptionPane;
+
+public class team222 {
 
     private static String[] arr;
 
+    public static void main(String[] args) {
+        System.out.println("Team 22");
+        System.out.print("Enter array size: ");
+        Scanner sc = new Scanner(System.in);
+        int siz = sc.nextInt();
+        arr = new String[siz];
 
-  public static void shiftArray()
-        {
-            String newArray="" , array="";
-            
-         for(int i=0 ; i<arr.length; i++)
-         {
-             array+= arr[i];
-         }
-        System.out.println("Old Array is: "); 
-        System.out.println(array);    
-        char temp; 
-        temp = array.charAt(0) ; 
-        for(int i=1 ; i<array.length(); i++)
-        {   
-            newArray+=array.charAt(i);
-           
-        }
-       newArray+=temp;
-        
-        System.out.println(newArray);
-        
-      
-        }
-    public static int[] ShuffleArray()
-    {
-        Random rgen = new Random();  // Random number generator
-
-        for (int i=0; i<arr.length; i++) {
-            int randomPosition = rgen.nextInt(arr.length);
-            int temp = arr[i];
-            arr[i] = arr[randomPosition];
-            arr[randomPosition] = temp;
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < siz; i++) {
+            arr[i] = sc.next();
         }
 
-        return array;
-    }
-
-
-     private static String[] arr;
-        Scanner sc=new Scanner(System.in);
         printMenu();
         int choice = sc.nextInt();
         switch (choice) {
@@ -67,11 +29,11 @@ public class Team22 {
                 break;
 
             case 2: //sort
-
+                sort_array();
                 break;
 
             case 3: //shuffle
-               ShuffleArray();    //20160011
+                ShuffleArray();    //20160011
                 break;
 
             case 5: //find smallest prime
@@ -81,63 +43,32 @@ public class Team22 {
 
                 break;
 
-
-
- 
             case 6: //check palindrome
-
+                palindrome();
                 break;
 
             case 7: //check sorted
                 checkSorted();
                 break;
 
-            case 8: //count primes
-                countPrimes(arr);
-         printMenu();
-         int choice = sc.nextInt();
-         switch (choice) {
-             case 1: //most repeated value
-                 break;
-             case 2: //sort
-		sort_array();
-                 break;
-             case 3: //shuffle
-
-                 break;
-             case 4: //find largest prime
-                 break;
-
-             case 5: //find smallest prime
-              leastPrime();
-                 break;
-             case 6: //check palindrome
-				palindrome();
-                 break;
-             case 7: //check sorted
-             checkSorted();
-
-                break;
-
-
             case 9: //reverse array
-
+                reversearray();
                 break;
 
+            case 8: //count primes
+                countPrimes();
+                break;
 
-             case 9: //reverse array
-            reversearray();
-              break;
             case 10: //shift array
-                shiftArray(arr);
+                shiftArray();
                 break;
-
 
             case 11: //distinct array
 
                 break;
-             case 12: //get the max 3 numbers
-				Greatest();
+
+            case 12: //get the max 3 numbers
+                Greatest();
                 break;
 
             case 13: //get the min 3 numbers
@@ -151,43 +82,17 @@ public class Team22 {
             case 15: //get median
 
                 break;
-
             case 16: //return only primes
 
                 break;
-
             case 17: //zero if less than zero
 
                 break;
-
             case 18: //execute all
-
                 break;
         }
-
- shiftArray
     }
 
-
-    public static void reversearray() {
-        int j = 0;
-        String ar[] = new String[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--)//do reverse
-        {
-            ar[j] = arr[i];
-            j++;
-        }
-        for (int i = 0; i < ar.length; i++)//print reverse arr
-        {
-            if (i == ar.length - 1) {
-                System.out.println(ar[i]);
-            } else {
-                System.out.print(ar[i] + ",");
-            }
-        }
-    }
-         
- master
     private static void printMenu() {
         System.out.println("Choose the number of operation you want to execute:");
         System.out.println("1-\tMost repeated value");
@@ -209,19 +114,36 @@ public class Team22 {
         System.out.println("17-\tZero if less than zero");
         System.out.println("18-\tExecute All");
 
-
     }
 
-    public static void countPrimes(String[] mynum) {
+    public static void reversearray() {
+        int j = 0;
+        String ar[] = new String[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--)//do reverse
+        {
+            ar[j] = arr[i];
+            j++;
+        }
+        for (int i = 0; i < ar.length; i++)//print reverse arr
+        {
+            if (i == ar.length - 1) {
+                System.out.println(ar[i]);
+            } else {
+                System.out.print(ar[i] + ",");
+            }
+        }
+    }
+
+    public static void countPrimes() {
         int counter = 0;
-        for (int i = 0; i < mynum.length; i++) {
-            if ((mynum[i].toCharArray()[0] >= 'a' && mynum[i].toCharArray()[0] <= 'z')
-                    || (mynum[i].toCharArray()[0] >= 'A' && mynum[i].toCharArray()[0] <= 'Z')) {
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i].toCharArray()[0] >= 'a' && arr[i].toCharArray()[0] <= 'z')
+                    || (arr[i].toCharArray()[0] >= 'A' && arr[i].toCharArray()[0] <= 'Z')) {
                 System.out.println("Function is applied on numbers only!");
                 break;
 
             } else {
-                if (isprime(Integer.parseInt(mynum[i]))) {
+                if (isprime(Integer.parseInt(arr[i]))) {
                     counter++;
                 } else {
                     counter += 0;
@@ -247,99 +169,89 @@ public class Team22 {
 
     }
 
-
-private static void leastPrime()
-	{	int lp = Integer.MAX_VALUE;
-		for (int l = 0; l < arr.length; ++l)
-	{
-		if ( Character.isLetter(arr[l].charAt(l)))
-		{
-			System.out.println("Function is applied on numbers only!");
-			break;
-		}
-      if (Integer.parseInt(arr[l]) == 2)
-	  {
-        lp = Integer.parseInt(arr[l]);
-        break;
-      }
-      if (Integer.parseInt(arr[l]) == 3)
-        lp = Integer.parseInt(arr[l]);
-      if (Integer.parseInt(arr[l]) % 2 == 0)
-        continue;
-      for (int i = 3; i <= (int) Math.sqrt(Integer.parseInt(arr[l])) + 1; i += 2)
-	  {
-        if (Integer.parseInt(arr[l]) % i == 0 && Integer.parseInt(arr[l]) != 3)
-          break;
-        if (lp >= Integer.parseInt(arr[l]))
-          lp = Integer.parseInt(arr[l]);
-      }
-    }
-    if (lp == Integer.MAX_VALUE)
-      System.out.println("No primes");
-    else
-      System.out.print(lp);
-	}
-
-
-             
-         }
-
-     }
-         
- public static void countPrimes() {
- 	int counter = 0;
- 	for (int i = 0; i<arr.length; i++) {
- 		if ( (arr[i].toCharArray()[0] >= 'a' && arr[i].toCharArray()[0]  <= 'z') ||
- 		           (arr[i].toCharArray()[0]  >= 'A' && arr[i].toCharArray()[0]  <= 'Z')) {
- 			System.out.println("Function is applied on numbers only!");
- 			break;
-
- 		}
- 		else {
- 				  if (isprime(Integer.parseInt(arr[i]))) {
- 					  counter++;
- 				  }
- 				  else {
- 					  counter+=0;
- 				  }
- 				
- 		}
- 			
- 		//System.out.println(counter);
- 		
- 	}
- 	if (counter!=0) {
- 	System.out.println(counter);
- 	        }
+    public static void checkSorted() {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].charAt(0) > arr[i + 1].charAt(0)) {
+                System.out.println("Array is not Sorted");
+                return;
+            }
         }
-         
-public static void palindrome(){
-        int reminder, sum = 0, temp;
-        int n = Integer.parseInt(JOptionPane.showInputDialog("Please enter your numbers: ")); 
-
-        temp = n;
-        while (n > 0) {
-            reminder = n % 10; 
-            sum = (sum * 10) + reminder;
-            n = n / 10;
-        }
-        if (temp == sum)
-            System.out.println("palindrome number ");
-        else
-            System.out.println("not palindrome");
+        System.out.println("Array is Sorted");
     }
-         
- public static boolean isprime(int n) {
- 	
- 	for(int i=2;i<n;i++) {
-         if(n%i==0)
-             return false;
-     }
-     return true;
- 	
- }
-         
- public static void leastPrime() {
+
+    public static void shiftArray() {
+        String newArray = "", array = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            array += arr[i];
+        }
+        System.out.println("Old Array is: ");
+        System.out.println(array);
+        char temp;
+        temp = array.charAt(0);
+        for (int i = 1; i < array.length(); i++) {
+            newArray += array.charAt(i);
+
+        }
+        newArray += temp;
+
+        System.out.println(newArray);
+
+    }
+
+    public static void Greatest() {
+        int x, y, z;
+        System.out.println("Enter three integers");
+        Scanner in = new Scanner(System.in);
+
+        x = in.nextInt();
+        y = in.nextInt();
+        z = in.nextInt();
+
+        if (x > y && x > z) {
+            System.out.println("First number is largest.");
+        } else if (y > x && y > z) {
+            System.out.println("Second number is largest.");
+        } else if (z > x && z > y) {
+            System.out.println("Third number is largest.");
+        } else {
+            System.out.println("The numbers are not distinct.");
+        }
+
+    }
+
+    public static void sort_array() {
+        /*Scanner scan = new Scanner(System.in);
+        System.out.println("enter the array size :");
+        int sz = scan.nextInt();
+        int[] input_array = new int[sz];
+        System.out.println("enter the array :");
+        for (int i = 0; i < sz; i++) {
+            input_array[i] = scan.nextInt();
+        }*/
+        Arrays.sort(arr);
+        System.out.println("the array after sort :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static int[] ShuffleArray() {
+        Random rgen = new Random();  // Random number generator
+
+        for (int i = 0; i < arr.length; i++) {
+            int randomPosition = rgen.nextInt(arr.length);
+            int temp = Integer.parseInt(arr[i]);
+            arr[i] = arr[randomPosition];
+            arr[randomPosition] = String.valueOf(temp);
+        }
+
+        return null;
+        //return array;
+    }
+
+    private static void leastPrime() {
         int lp = Integer.MAX_VALUE;
         for (int l = 0; l < arr.length; ++l) {
             if (Character.isLetter(arr[l].charAt(l))) {
@@ -370,79 +282,23 @@ public static void palindrome(){
         } else {
             System.out.print(lp);
         }
-    }  
-    
-
-    public static void checkSorted(){
-        for(int i = 0; i<arr.length -1 ; i++){
-            if(arr[i].charAt(0) >  arr[i + 1].charAt(0))
-            {
-                System.out.println("Array is not Sorted");
-                return;
-            }
-        }
-         System.out.println("Array is Sorted");
     }
- public static void shiftArray(String[] x)
-        {
-            String newArray="" , array="";
 
+    public static void palindrome() {
+        int reminder, sum = 0, temp;
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Please enter your numbers: "));
 
-         for(int i=0 ; i<x.length; i++)
-         {
-             array+= x[i];
-         }
-        System.out.println("Old Array is: ");
-        System.out.println(array);
-        char temp;
-        temp = array.charAt(0) ;
-        for(int i=1 ; i<array.length(); i++)
-        {
-            newArray+=array.charAt(i);
-
+        temp = n;
+        while (n > 0) {
+            reminder = n % 10;
+            sum = (sum * 10) + reminder;
+            n = n / 10;
         }
-       newArray+=temp;
-
-        System.out.println(newArray);
-
-
+        if (temp == sum) {
+            System.out.println("palindrome number ");
+        } else {
+            System.out.println("not palindrome");
         }
+    }
 
 }
-public static void Greatest() {
-	 int x, y, z;
-    System.out.println("Enter three integers");
-    Scanner in = new Scanner(System.in);
- 
-    x = in.nextInt();
-    y = in.nextInt();
-    z = in.nextInt();
- 
-    if (x > y && x > z)
-      System.out.println("First number is largest.");
-    else if (y > x && y > z)
-      System.out.println("Second number is largest.");
-    else if (z > x && z > y)
-      System.out.println("Third number is largest.");
-    else
-      System.out.println("The numbers are not distinct.");
-
-}
-
- public static void sort_array (){
-            Scanner scan = new Scanner (System.in);
-            System.out.println("enter the array size :");
-            int sz = scan.nextInt();
-            int [] input_array = new int [sz];
-            System.out.println("enter the array :");
-            for (int i = 0 ; i < sz ;i++){
-                input_array[i] = scan.nextInt();
-            }
-            Arrays.sort(input_array);
-            System.out.println("the array after sort :");
-            for (int i = 0 ; i < sz ;i++){
-                System.out.print(input_array[i]+" ");
-            }
-            System.out.println("");
-     }
-
